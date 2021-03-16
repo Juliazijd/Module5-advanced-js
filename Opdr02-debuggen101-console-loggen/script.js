0cef82871bb7fc4a05eb64fe7e73f74214c65f9e
Julia Zijderveld <juliazijderveld@hotmail.com>
2021-03-16 15:01:35 +0100

    added exercise 2 and changed folder structure of exercise 1

---
+++ Opdr02-debuggen101-console-loggen/script.js
@@ -0,0 +1,21 @@
+const array = [
+  { name: "N. Armstrong", profession: "spacecowboy", age: 89 },
+  { name: "H. de Haan", profession: "kippen hypnotiseur", age: 59 },
+  { name: "A. Curry", profession: "kikvorsman", age: 32 },
+  { name: "F. Vonk", profession: "slangenmelker", age: 36 },
+  { name: "B. Bunny", profession: "konijnen uitlaatservice", age: 27 },
+  { name: "Dr.Evil", profession: "digital overlord", age: 56 }
+];
+
+for (let person of array) {
+  console.log("Dit is een persoon: ", [person]);
+  console.log("De naam van een persoon: ", [person.name]);
+  console.log("Dit is het geboortejaar van de persoon: ", (2021 - [person.age]));
+  console.log(`${person.name} is ${person.profession}`);
+
+  if (person.age > 50) {
+      console.log(true);
+  } else {
+      console.log(false)
+  }
+}
