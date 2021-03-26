20127766cc95e74a78ccb1f2c1193b05150f9a00
Julia Zijderveld <juliazijderveld@hotmail.com>
2021-03-26 12:58:09 +0100

    Added exercise 7: practice with promise

--- Opdr-js-callback/script.js
+++
@@ -1,9 +0,0 @@
-const huiswerkMaken = (vak) => {
- console.log(`Ok, ok, ik ga nu mijn ${vak} huiswerk maken`)
-}
-
-const klaarMetHuiswerk = () => {
-   setTimeout(() => { console.log(`Kijk Paps/Mams, ik ben klaar met mijn huiswerk!`) }, 2000) 
-}
-
-huiswerkMaken('wiskunde', klaarMetHuiswerk());
